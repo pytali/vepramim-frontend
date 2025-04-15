0cb013610542aaf2c1d4c964f4468f8979d03978
Walisson Gois <walissongois@gmail.com>
2025-04-14 23:47:19 -0400

    refactor: Remove função de rewrites do Next.js, simplificando a configuração e eliminando a necessidade de prefixo nas rotas

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: 'standalone',
-  async rewrites() {
-    return [
-      {
-        source: '/onu/:path*',
-        destination: '/:path*',
-      },
-    ]
-  },
 }
 
 export default nextConfig
